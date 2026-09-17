808f58dc78e8d54b81d575d7d2dc52daa82c159c
Juhun Lee <rangho@yonsei.ac.kr>
2026-09-17 16:41:59 +0900

    chore: separate svelte config out from vite

---
+++ svelte.config.js
@@ -0,0 +1,13 @@
+import adapter from "@sveltejs/adapter-static";
+
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import { mdsvex } from "mdsvex";
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  extensions: [".svelte", ".md"],
+  preprocess: [mdsvex({ extensions: [".md"] }), vitePreprocess()],
+  kit: { adapter: adapter() },
+};
+
+export default config;
